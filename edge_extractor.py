2751f9bbbd8c400a47fc26ab5a3fb75262f32702
muelleram <mueller.amelie@posteo.de>
2023-10-25 11:14:17 +0200

    Remove "node_timeline: bool" from build_edge_timeline() as it is not used in method, improve documentation of edge_extractor.py

--- edge_extractor.py
+++ edge_extractor.py
@@ -30,6 +30,12 @@ class Edge:
 
 
 class EdgeExtracter(TemporalisLCA):
+    """
+    Child class of TemporalisLCA that traverses the supply chain just as the parent class but can create a timeline of edges, in addition timeline of flows or nodes.
+    The edge timeline is then used to match the timestamp of edges to that of background databases and to replace these edges with edges from these background databases using Brightway Datapackages
+    The current implementation is work in progress and can only handle temporal distributions in the foreground.
+
+    """
     def __init__(self, *args, edge_filter_function: Callable = None, **kwargs):
         super().__init__(*args, **kwargs)
         if edge_filter_function:
@@ -37,11 +43,19 @@ class EdgeExtracter(TemporalisLCA):
         else:
             self.edge_ff = lambda x: False
 
-    def build_edge_timeline(self, node_timeline: bool | None = False) -> List:
+    def build_edge_timeline(self) -> List:
+        """
+        Creates a timeline of the edges from the graph traversal. Starting from the edges of the functional unit node, it goes through each node using a heap, 
+        and propagates the TemporalDistributions of the edges from node to node through time using convolution-operators during multiplication. 
+        It stops in case the current edge is known to have no temporal distribution (=leaf) (e.g. part of background database).
+
+        :return: A list of Edge instances with timestamps and amounts, and ids of its producing and consuming node.
+        -------------------
+        """
         heap = []
         timeline = []
 
-        for edge in self.edge_mapping[self.unique_id]:
+        for edge in self.edge_mapping[self.unique_id]: # starting at the edges of the functional unit
             node = self.nodes[edge.producer_unique_id]
             heappush(
                 heap,
@@ -70,19 +84,19 @@ class EdgeExtracter(TemporalisLCA):
                     input_id=row_id,
                     output_id=col_id,
                 )
-                value = (
+                value = (                           # value is the TemporalDistribution of the edge
                     self._exchange_value(
                         exchange=exchange,
                         row_id=row_id,
                         col_id=col_id,
                         matrix_label="technosphere_matrix",
                     )
-                    / node.reference_product_production_amount
+                    / node.reference_product_production_amount 
                 )
                 producer = self.nodes[edge.producer_unique_id]
                 leaf = self.edge_ff(row_id)
 
-                distribution = (td * value).simplify()
+                distribution = (td * value).simplify() # convolution-multiplication of TemporalDistribution of consuming node (td) and consumed edge (edge) gives TD of producing node
                 timeline.append(
                     Edge(
                         distribution=distribution,
