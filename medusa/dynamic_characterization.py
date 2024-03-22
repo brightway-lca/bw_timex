b14af1ca797d459d82bb899438dbee45f517b1fd
TimoDiepers <timo.diepers@gmail.com>
2024-03-22 22:32:53 +0100

    add updated dyn cf functions for co2 and ch4

--- medusa/dynamic_characterization.py
+++ medusa/dynamic_characterization.py
@@ -4,7 +4,7 @@ import pandas as pd
 import bw2data as bd
 import numpy as np
 import os
-from bw_temporalis.lcia.climate import characterize_methane, characterize_co2
+# from bw_temporalis.lcia.climate import characterize_methane, characterize_co2
 from datetime import datetime, timedelta
 
 
@@ -156,7 +156,7 @@ class DynamicCharacterization():
         if type_of_method not in {"radiative_forcing", "GWP"}:
             raise ValueError(f"impact assessment type must be either 'radiative_forcing' or 'GWP', not {type_of_method}")	
         
-        characterization_dictionary = {"carbon dioxide": characterize_co2, "methane": characterize_methane, "carbon monoxide": self.characterize_co, "nitrous oxide": self.characterize_n2o}
+        characterization_dictionary = {"carbon dioxide": self.characterize_co2, "methane": self.characterize_ch4, "carbon monoxide": self.characterize_co, "nitrous oxide": self.characterize_n2o}
         #TODO think if it makes sense to store characterization dictionary here
         mapping_flow_to_id = {flow: bd.get_activity(name=flow).id for flow in characterization_dictionary.keys()}
 
@@ -272,6 +272,166 @@ class DynamicCharacterization():
         
         return all_characterized_inventory, type_of_method, fixed_TH, TH
        
+       
+    def characterize_co2(self,
+    series,
+    period: int | None = 100,
+    cumulative: bool | None = False,
+    ) -> pd.DataFrame:
+        """
+        Based on characterize_co2 from bw_temporalis, but updated numerical values from IPCC AR6 Ch7 & SM.
+        
+        Calculate the cumulative or marginal radiative forcing (CRF) from CO2 for each year in a given period.
+
+        If `cumulative` is True, the cumulative CRF is calculated. If `cumulative` is False, the marginal CRF is calculated.
+        Takes a single row of the TimeSeries Pandas DataFrame (corresponding to a set of (`date`/`amount`/`flow`/`activity`).
+        For each year in the given period, the CRF is calculated.
+        Units are watts/square meter/kilogram of CO2.
+
+        Returns
+        -------
+        A TimeSeries dataframe with the following columns:
+        - date: datetime64[s]
+        - amount: float
+        - flow: str
+        - activity: str
+
+        Notes
+        -----
+        See also the relevant scientific publication on CRF: https://doi.org/10.5194/acp-13-2793-2013
+        See also the relevant scientific publication on the numerical calculation of CRF: http://pubs.acs.org/doi/abs/10.1021/acs.est.5b01118
+        Numerical values from IPCC AR6 Chapter 7
+
+        See Also
+        --------
+        characterize_methane: The same function for CH4
+        """
+
+        # functional variables and units (from publications listed in docstring)
+        radiative_efficiency_ppb = 1.33e-5 # W/m2/ppb; 2019 background co2 concentration; IPCC AR6 Table 7.15 
+        
+        # for conversion from ppb to kg-CO2
+        M_co2 = 44.01 # g/mol
+        M_air = 28.97 # g/mol, dry air
+        m_atmosphere = 5.135e18 # kg [Trenberth and Smith, 2005]
+        
+        radiative_efficiency_kg = radiative_efficiency_ppb * M_air / M_co2 * 1e9 / m_atmosphere # W/m2/kg-CO2
+        
+        alpha_0, alpha_1, alpha_2, alpha_3 = 0.2173, 0.2240, 0.2824, 0.2763
+        tau_1, tau_2, tau_3 = 394.4, 36.54, 4.304
+        decay_term = lambda year, alpha, tau: alpha * tau * (1 - np.exp(-year / tau))
+
+        date_beginning: np.datetime64 = series["date"].to_numpy()
+        date_characterized: np.ndarray = date_beginning + np.arange(
+            start=0, stop=period, dtype="timedelta64[Y]"
+        ).astype("timedelta64[s]")
+
+        decay_multipliers: np.ndarray = np.array(
+            [
+                radiative_efficiency_kg
+                * (
+                    alpha_0 * year
+                    + decay_term(year, alpha_1, tau_1)
+                    + decay_term(year, alpha_2, tau_2)
+                    + decay_term(year, alpha_3, tau_3)
+                )
+                for year in range(period)
+            ]
+        )
+
+        forcing = pd.Series(data=series.amount * decay_multipliers, dtype="float64")
+        if not cumulative:
+            forcing = forcing.diff(periods=1).fillna(0)
+
+        return pd.DataFrame(
+            {
+                "date": pd.Series(data=date_characterized, dtype="datetime64[s]"),
+                "amount": forcing,
+                "flow": series.flow,
+                "activity": series.activity,
+            }
+        )
+    
+    
+    def characterize_ch4(self, series, period: int = 100, cumulative=False) -> pd.DataFrame:
+        """
+        Based on characterize_methane from bw_temporalis, but updated numerical values from IPCC AR6 Ch7 & SM.
+        
+        Calculate the cumulative or marginal radiative forcing (CRF) from CH4 for each year in a given period.
+
+        If `cumulative` is True, the cumulative CRF is calculated. If `cumulative` is False, the marginal CRF is calculated.
+        Takes a single row of the TimeSeries Pandas DataFrame (corresponding to a set of (`date`/`amount`/`flow`/`activity`).
+        For earch year in the given period, the CRF is calculated.
+        Units are watts/square meter/kilogram of CH4.
+
+        Parameters
+        ----------
+        series : array-like
+            A single row of the TimeSeries dataframe.
+        period : int, optional
+            Time period for calculation (number of years), by default 100
+        cumulative : bool, optional
+            Should the RF amounts be summed over time?
+
+        Returns
+        -------
+        A TimeSeries dataframe with the following columns:
+        - date: datetime64[s]
+        - amount: float
+        - flow: str
+        - activity: str
+
+        Notes
+        -----
+        See also the relevant scientific publication on CRF: https://doi.org/10.5194/acp-13-2793-2013
+        See also the relevant scientific publication on the numerical calculation of CRF: http://pubs.acs.org/doi/abs/10.1021/acs.est.5b01118
+
+        See Also
+        --------
+        characterize_co2: The same function for CO2
+        """
+
+        # functional variables and units (from publications listed in docstring)
+        radiative_efficiency_ppb = 3.89e-4 # W/m2/ppb; 2019 background cch4 concentration; IPCC AR6 Table 7.15 
+
+        # for conversion from ppb to kg-CH4
+        M_ch4 = 16.04 # g/mol
+        M_air = 28.97 # g/mol, dry air
+        m_atmosphere = 5.135e18 # kg [Trenberth and Smith, 2005]
+
+        radiative_efficiency_kg = radiative_efficiency_ppb * M_air / M_ch4 * 1e9 / m_atmosphere # W/m2/kg-CH4; 
+        
+        # TODO I dont really know where this is coming from and what f1 and f2 do. Maybe they account for the decay of CH4 to other GHGs?
+        f1 = 0.5  # Unitless
+        f2 = 0.15  # Unitless
+        tau = 12.4  # Lifetime (years)
+
+        date_beginning: np.datetime64 = series["date"].to_numpy()
+        date_characterized: np.ndarray = date_beginning + np.arange(
+            start=0, stop=period, dtype="timedelta64[Y]"
+        ).astype("timedelta64[s]")
+
+        decay_multipliers: list = np.array(
+            [
+                (1 + f1 + f2) * radiative_efficiency_kg * tau * (1 - np.exp(-year / tau))
+                for year in range(period)
+            ]
+        )
+
+        forcing = pd.Series(data=series.amount * decay_multipliers, dtype="float64")
+        if not cumulative:
+            forcing = forcing.diff(periods=1).fillna(0)
+
+        return pd.DataFrame(
+            {
+                "date": pd.Series(data=date_characterized, dtype="datetime64[s]"),
+                "amount": forcing,
+                "flow": series.flow,
+                "activity": series.activity,
+            }
+        )
+    
+    
     def characterize_co(self, series, period: int = 100, cumulative=False) -> pd.DataFrame:
         """
         Radiative forcing function of CO from Levasseur et al 2010 (self.levasseur_dcfs["carbon monoxide"])
