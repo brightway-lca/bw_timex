395f6e101d1636727f68268110103603d03be8c7
Timo Diepers <90762029+TimoDiepers@users.noreply.github.com>
2024-03-27 08:26:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tictac_lca',
+    name='timex_lca',
     version='0.1',
     packages=find_packages(),
     # Add other arguments as needed
