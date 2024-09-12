f8653363fe77b6270a236ef99f165ce734669984
TimoDiepers <timo.diepers@gmail.com>
2024-09-12 16:27:58 +0200

    update links

--- docs/conf.py
+++ docs/conf.py
@@ -183,7 +183,7 @@ html_theme_options = {
     "icon_links": [
         {
             "name": "Launch interactive Demo on Binder",
-            "url": "https://mybinder.org/v2/gh/brightway-lca/bw_timex/HEAD?labpath=notebooks%2Fexample_electric_vehicle_standalone.ipynb",
+            "url": "https://mybinder.org/v2/gh/brightway-lca/bw_timex/HEAD?labpath=notebooks%2Fgetting_started.ipynb",
             "icon": "fa-solid fa-rocket",
         },
         {
