f785cb2c063a0bd38c58352dbfc939b86b7fda14
TimoDiepers <timo.diepers@gmail.com>
2026-08-24 16:26:38 +0200

    release: v1.3.0

--- bw_timex/__init__.py
+++ bw_timex/__init__.py
@@ -24,7 +24,7 @@ from .utils import (
     plot_characterized_inventory_as_waterfall,
 )
 
-__version__ = "1.2.1"
+__version__ = "1.3.0"
 
 __all__ = [
     # forwarded from bw_temporalis for convenience
